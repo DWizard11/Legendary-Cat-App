3f2f36871bcca0a95334e51a6dca660d0ef23ce6
DWizard11 <daniyalisawesome111@gmail.com>
2022-07-16 10:55:13 +0800

    Initial Commit

---
+++ Cat App/Cat_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_AppApp.swift
+//  Cat App
+//
+//  Created by DWizard11 on 16/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Cat_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cat App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cat App
+//
+//  Created by DWizard11 on 16/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
